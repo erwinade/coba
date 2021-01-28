4b1fcb2652d2c388910b1103865242328006621c
gatot <gearroboone@gmail.com>
2021-01-28 14:04:46 +0700

    update fetch insert select

---
+++ insert.php
@@ -0,0 +1,57 @@
+<?php  
+ include 'conn.php';
+ if(!empty($_POST))  
+ {  
+     $output = '';  
+     $message = '';  
+     $sens_name = mysqli_real_escape_string($conn, $_POST["sens_name"]);  
+     $sens_type = mysqli_real_escape_string($conn, $_POST["sens_type"]);  
+     $threshold_min = mysqli_real_escape_string($conn, $_POST["threshold_min"]);  
+     $sens_value = mysqli_real_escape_string($conn, $_POST["sens_value"]);  
+     $threshold_max = mysqli_real_escape_string($conn, $_POST["threshold_max"]);  
+     if($_POST["employee_id"] != '')  
+     {  
+          $query = "  UPDATE sensor   
+          SET sens_name='$sens_name',   
+          sens_type='$sens_type',   
+          threshold_min='$threshold_min',   
+          sens_value = '$sens_value',   
+          threshold_max = '$threshold_max'
+          WHERE id='".$_POST["employee_id"]."'";  
+          $messthreshold_max = 'Data Updated';  
+     }  
+     else  
+     {  
+          $query = "INSERT INTO sensor(sens_name, sens_type, threshold_min, sens_value, threshold_max)  
+          VALUES('$sens_name', '$sens_type', '$threshold_min', '$sens_value', '$threshold_max');  
+          ";  
+          $message = 'Data Inserted';  
+     }  
+     if(mysqli_query($conn, $query))  
+     {  
+          $output .= '<label class="text-success">' . $message . '</label>';  
+          $select_query = "SELECT * FROM sensor ORDER BY id DESC";  
+          $result = mysqli_query($conn, $select_query);  
+          $output .= '  
+               <table class="table table-bordered">  
+                    <tr>  
+                         <th width="70%">Employee Name</th>  
+                         <th width="15%">Edit</th>  
+                         <th width="15%">View</th>  
+                    </tr>  
+          ';  
+          while($row = mysqli_fetch_array($result))  
+          {  
+               $output .= '  
+                    <tr>  
+                         <td>' . $row["sens_name"] . '</td>  
+                         <td><input type="button" name="edit" value="Edit" id="'.$row["id"] .'" class="btn btn-info btn-xs edit_data" /></td>  
+                         <td><input type="button" name="view" value="view" id="' . $row["id"] . '" class="btn btn-info btn-xs view_data" /></td>  
+                    </tr>  
+               ';  
+          }  
+          $output .= '</table>';  
+     }  
+     echo $output;  
+}  
+?>
---
+++ select.php
@@ -0,0 +1,55 @@
+<?php  
+  include 'conn.php';
+ if(isset($_POST["employee_id"]))  
+ {   
+      $satuan='';
+      $output = '';   
+      $query = "SELECT * FROM sensor WHERE id = '".$_POST["employee_id"]."'";  
+      $result = mysqli_query($conn, $query);  
+      $output .= '  
+      <div class="table-responsive">  
+           <table class="table table-bordered">';  
+      while($row = mysqli_fetch_array($result))  
+      {  
+           if ($row['sens_type'] == "temp")
+           {
+                $satuan = "C";
+           }
+           if ($row['sens_type'] == "hum")
+           {
+                $satuan = "%";
+           }
+           $output .= '  
+                <tr>  
+                     <td width="30%"><label>Name</label></td>  
+                     <td width="70%">'.$row["sens_name"].'</td>  
+                </tr>  
+                <tr>  
+                     <td width="30%"><label>Type</label></td>  
+                     <td width="70%">'.$row["sens_type"].'</td>  
+                </tr>  
+                <tr>  
+                     <td width="30%"><label>Threshold Min</label></td>  
+                     <td width="70%">'.$row["threshold_min"].'</td>  
+                </tr>  
+                <tr>  
+                     <td width="30%"><label>Threshold Max</label></td>  
+                     <td width="70%">'.$row["threshold_max"].'</td>  
+                </tr>  
+                <tr>  
+                     <td width="30%"><label>Value</label></td>  
+                     <td width="70%">'.$row["sens_value"].'&nbsp;'.$satuan.'</td>  
+                </tr>  
+                <tr>  
+                     <td width="30%"><label>Status</label></td>  
+                     <td width="70%">'.$row["status_sensor"].'</td>  
+                </tr>  
+           ';  
+      }  
+      $output .= '  
+           </table>  
+      </div>  
+      ';  
+      echo $output;  
+ }  
+ ?>
