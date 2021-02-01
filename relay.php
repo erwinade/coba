b3ca3eaeebd3980f980ace638b61d1435d698b6d
gatot <gearroboone@gmail.com>
2021-02-02 00:34:27 +0700

    relay lagi

--- relay.php
+++ relay.php
@@ -11,7 +11,8 @@ if (isset($_POST['relay1on']))
     system(" gpio mode 25 out ") ; 
     system(" gpio write 25 1") ; 
     $relay1=1;
-    $sql1 = "UPDATE sensor SET sens_value='$relay1' WHERE id='$id_rly1'";
+    $sts_rly1="Close";
+    $sql1 = "UPDATE sensor SET sens_value='$relay1', status_sensor='$sts_rly1' WHERE id='$id_rly1'";
     if ($conn->query($sql1) === TRUE) {
         header('location: detail.php');
       } else {
@@ -24,7 +25,8 @@ else if (isset($_POST['relay1off']))
     system(" gpio mode 25 out ") ; 
     system(" gpio write 25 0") ; 
     $relay1=0;
-    $sql1 = "UPDATE sensor SET sens_value='$relay1' WHERE id='$id_rly1'";
+    $sts_rly1="Open";
+    $sql1 = "UPDATE sensor SET sens_value='$relay1', status_sensor='$sts_rly1' WHERE id='$id_rly1'";
     if ($conn->query($sql1) === TRUE) {
         header('location: detail.php');
       } else {
@@ -37,7 +39,8 @@ if (isset($_POST['relay2on']))
     system(" gpio mode 8 out ") ; 
     system(" gpio write 8 1") ; 
     $relay2=1;
-    $sql2 = "UPDATE sensor SET sens_value='$relay2' WHERE id='$id_rly2'";
+    $sts_rly2="Close";
+    $sql2 = "UPDATE sensor SET sens_value='$relay2', status_sensor='$sts_rly2' WHERE id='$id_rly2'";
     if ($conn->query($sql2) === TRUE) {
         header('location: detail.php');
       } else {
@@ -50,7 +53,8 @@ else if (isset($_POST['relay2off']))
     system(" gpio mode 8 out ") ; 
     system(" gpio write 8 0") ; 
     $relay2=0;
-    $sql2 = "UPDATE sensor SET sens_value='$relay2' WHERE id='$id_rly2'";
+    $sts_rly2="Open";
+    $sql2 = "UPDATE sensor SET sens_value='$relay2', status_sensor='$sts_rly2' WHERE id='$id_rly2'";
     if ($conn->query($sql2) === TRUE) {
         header('location: detail.php');
       } else {
@@ -63,7 +67,8 @@ if (isset($_POST['relay3on']))
     system(" gpio mode 7 out ") ; 
     system(" gpio write 7 1") ; 
     $relay3=1;
-    $sql3 = "UPDATE sensor SET sens_value='$relay3' WHERE id='$id_rly3'";
+    $sts_rly3="Close";
+    $sql3 = "UPDATE sensor SET sens_value='$relay3', status_sensor='$sts_rly3' WHERE id='$id_rly3'";
     if ($conn->query($sql3) === TRUE) {
         header('location: detail.php');
       } else {
@@ -76,7 +81,8 @@ else if (isset($_POST['relay3off']))
     system(" gpio mode 7 out ") ; 
     system(" gpio write 7 0") ; 
     $relay3 = 0 ;
-    $sql3 = "UPDATE sensor SET sens_value='$relay3' WHERE id='$id_rly3'";
+    $sts_rly3="Open";
+    $sql3 = "UPDATE sensor SET sens_value='$relay3', status_sensor='$sts_rly3' WHERE id='$id_rly3'";
     if ($conn->query($sql3) === TRUE) {
         header('location: detail.php');
       } else {
@@ -89,7 +95,8 @@ if (isset($_POST['relay4on']))
     system(" gpio mode 1 out ") ; 
     system(" gpio write 1 1") ; 
     $relay4=1;
-    $sql4 = "UPDATE sensor SET sens_value='$relay4' WHERE id='$id_rly4'";
+    $sts_rly4="Close";
+    $sql4 = "UPDATE sensor SET sens_value='$relay4', status_sensor='$sts_rly4' WHERE id='$id_rly4'";
     if ($conn->query($sql4) === TRUE) {
         header('location: detail.php');
       } else {
@@ -102,7 +109,8 @@ else if (isset($_POST['relay4off']))
     system(" gpio mode 1 out ") ; 
     system(" gpio write 1 0") ; 
     $relay4=0;
-    $sql4 = "UPDATE sensor SET sens_value='$relay4' WHERE id='$id_rly4'";
+    $sts_rly4="Open";
+    $sql4 = "UPDATE sensor SET sens_value='$relay4', status_sensor='$sts_rly4' WHERE id='$id_rly4'";
     if ($conn->query($sql4) === TRUE) {
         header('location: detail.php');
       } else {
@@ -111,19 +119,6 @@ else if (isset($_POST['relay4off']))
     //echo "off";
 }
 
-/*$relay1 = system("gpio read 25");
-$relay2 = system("gpio read 8");
-$relay3 = system("gpio read 7");
-$relay4 = system("gpio read 1");*/
-//echo "relay 1="; echo $relay1;
-//echo "relay 2="; echo $relay2;
-//echo "relay 3="; echo $relay3;
-//echo "relay 4="; echo $relay4;
-
-//$sql1 = "UPDATE sensor SET sens_value='$relay1' WHERE id='$id_rly1'";
-
-
-//$sql4 = "UPDATE sensor SET sens_value='$relay4' WHERE id='$id_rly4'";
 
 $conn->close();
 ?>
