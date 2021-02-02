42fb74f1aaf0409edd3e4292951cf9b2901c4698
gatot <gearroboone@gmail.com>
2021-02-02 14:42:56 +0700

    update setting

--- setting.php
+++ setting.php
@@ -183,27 +183,31 @@
                                                         {
                                                             $id = $internal['id'];
                                                             $description = $internal['name'];
-                                                            $email = $internal['email'];
+                                                            $email = $internal['email_user'];
                                                             $smtp = $internal['smtp'];
                                                     ?>
+                                                    <form method='post' action='aksi.php'>
                                                         <table id="datatable" class="table table-striped table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
                                                                 <tr>
                                                                     <th>Nama</th>
-                                                                    <td><?php echo $description; ?></td>
+                                                                    <td><input type="text" class="form-control" id="Emailname" required="required" name="emailname" placeholder="<?php echo $description; ?>"></td>
                                                                 </tr>
                                                                 <tr>
                                                                     <th>Email</th>
-                                                                    <td><?php echo $email; ?></td>
+                                                                    <td><input type="text" class="form-control" id="Email_user" required="required" name="email_user" placeholder="<?php echo $email; ?>"></td>
                                                                 </tr>
                                                                 <tr>
                                                                     <th>SMPT</th>
-                                                                    <td><?php echo $smtp; ?></td>
+                                                                    <td><input type="text" class="form-control" id="Smtp" required="required" name="smtp" placeholder="<?php echo $smtp; ?>"></td>
                                                                 </tr>
                                                             </tbody>
                                                             <?php
                                                                 }
                                                             ?>
                                                         </table>
+                                                        <br>
+                                                        <button type="submit" name="email" class="btn btn-info">Submit</button>
+                                                    </form>
                                                     </div>
                                                 </div>
                                             </div>
@@ -242,20 +246,24 @@
                                                             $description = $internal['name'];
                                                             $bot = $internal['bot_id'];
                                                     ?>
+                                                    <form method="post" action="aksi.php">
                                                         <table id="datatable" class="table table-striped table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
                                                                 <tr>
                                                                     <th>Nama</th>
-                                                                    <td><?php echo $description; ?></td>
+                                                                    <td><input type="text" class="form-control" id="Telename" required="required" name="telename" placeholder="<?php echo $description; ?>"></td>
                                                                 </tr>
                                                                 <tr>
                                                                     <th>Bot_Id</th>
-                                                                    <td><?php echo $bot; ?></td>
+                                                                    <td><input type="text" class="form-control" id="Bot" required="required" name="bot" placeholder="<?php echo $bot; ?>"></td>
                                                                 </tr>
                                                             </tbody>
                                                             <?php
                                                                 }
                                                             ?>
                                                         </table>
+                                                        <br>
+                                                        <button type="submit" name="tele" class="btn btn-info">Submit</button>
+                                                    </form>
                                                     </div>
                                                 </div>
                                             </div>
@@ -271,20 +279,6 @@
                     </div>
                     <!--end col-->
                 </div>
-
-               
-
-                <div class="row">
-                    <div class="col-lg-12">
-                            <div class="card-body">
-                                <button class="btn btn-primary mb-4" a href="aksi.php">Submit</button>
-                            </div>
-                            <!--end card-body-->
-                        <!--end card-->
-                    </div>
-                </div>
-
-                
                 <!-- ============================================================== -->
                 <!-- End Right sidebar -->
                 <!-- ============================================================== -->
