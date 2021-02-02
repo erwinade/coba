3ea4d83e77ec158b357dda32ac1bc4e3a5570946
gatot <gearroboone@gmail.com>
2021-02-02 14:44:18 +0700

    update aksi

--- aksi.php
+++ aksi.php
@@ -28,6 +28,38 @@ if(isset($_POST['dvc'])){ //['ttambah'] merupakan name dari button di form tamba
 		echo "Error: " . $sql . "<br>" . $conn->error;
 	}
 }
+
+if(isset($_POST['email'])){ //['ttambah'] merupakan name dari button di form tambah
+	$Username	= $_POST['emailname'];
+	$Email 		= $_POST['email_user'];
+	$Smtp		= $_POST['smtp'];
+	
+	$sql	= $query="UPDATE email SET name='$Username', email_user='$Email', smtp='$Smtp' where id = 1";
+	$query	= mysqli_query($conn,$sql);
+	
+	if($query){
+		header('location: setting.php'); //kode ini supaya jika data setelah ditambahkan form kembali menuju tabel data siswa
+	}
+	else{
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+}
+
+if(isset($_POST['tele'])){ //['ttambah'] merupakan name dari button di form tambah
+	$telename	= $_POST['telename'];
+	$bot	= $_POST['bot'];
+	
+	$sql	= $query="UPDATE telegram SET name='$telename', bot_id='$bot' where id = 1";
+	$query	= mysqli_query($conn,$sql);
+	
+	if($query){
+		header('location: setting.php'); //kode ini supaya jika data setelah ditambahkan form kembali menuju tabel data siswa
+	}
+	else{
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+}
+
 $conn->close();
 
 	
