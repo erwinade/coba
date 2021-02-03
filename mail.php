1e4cd074abd3007271f7bfe49f0cb6e0e926559b
gatot <gearroboone@gmail.com>
2021-02-03 23:00:33 +0700

    kirim email

---
+++ mail.php
@@ -0,0 +1,29 @@
+<?php
+include "classes/class.phpmailer.php";
+include './conn.php';
+
+$sql	= 'select * from email where id = "1"';
+$query	= mysqli_query($conn,$sql);
+while($row = mysqli_fetch_array($query))
+{
+    $email_name = $row['email_name'];
+    $email_user = $row['email_user'];
+    $pswd = $row['password'];
+    $smtp = $row['smtp'];
+}
+$mail = new PHPMailer; 
+$mail->IsSMTP();
+$mail->SMTPSecure = 'ssl'; 
+$mail->Host = $smtp; //host masing2 provider email
+$mail->SMTPDebug = 2;
+$mail->Port = 465;
+$mail->SMTPAuth = true;
+$mail->Username = $email_user; //user email
+$mail->Password = $pswd; //password email 
+$mail->SetFrom($email_user,"Nama pengirim"); //set email pengirim
+$mail->Subject = "Testing"; //subyek email
+//$mail->AddAddress("soendev@gmail.com","nama email tujuan");  //tujuan email
+$mail->MsgHTML("Testing...");
+if($mail->Send()) echo "Message has been sent";
+else echo "Failed to sending message";
+?>
