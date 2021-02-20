738a650aba4391d3587b9c3846a7be87498dee30
gatot <gearroboone@gmail.com>
2021-02-20 16:01:56 +0700

    update mail

--- mail.php
+++ mail.php
@@ -22,37 +22,52 @@ $msg = "";
 if (mysqli_num_rows($result) > 0) {
     // append data of each row to $msg.
     while($row = mysqli_fetch_assoc($result)) {
-        $msg .= "Description: " . $row["sens_name"] . " - Type Sensor: " . $row["sens_type"]. " - Value: " . $row["sens_value"] . " - Timestamp: " .$row["created_at"]."<br>";
+        $msg .= "Description: " . $row["sens_name"] . " - Type Sensor: " . $row["sens_type"]. " - Value: " . $row["sens_value"] . " - Timestamp: " .$row["update_at"]."<br>";
         $id = $row["id"];
+        $send_at = strtotime($row["send_at"]);
+        $wktu_at = strtotime($wktu);
+        $timekeep = $wktu_at - $send_at;
+        echo $timekeep."<br>";
+        echo $send_at;
 
     }
-    
-    $mail = new PHPMailer; 
-    $mail->IsSMTP();
-    $mail->SMTPSecure = $smtps; 
-    $mail->Host = $smtp; //host masing2 provider email
-    $mail->SMTPDebug = 2;
-    $mail->Port = $port;
-    $mail->SMTPAuth = true;
-    $mail->Username = $email_user; //user email
-    $mail->Password = $pswd; //password email 
-    $mail->SetFrom($email_user,"Head of IT Engginer"); //set email pengirim
-    $mail->Subject = "Alert MONSiS-8 THD"; //subyek email
-    $mail->AddAddress($email_to,"Admin MONSiS-8THD");  //tujuan email
-    $mail->MsgHTML($msg);
-    if($mail->Send()) 
+    if ( $timekeep > 1801 )
     {
-        $sql = "UPDATE sensor SET kirim_at='$wktu' WHERE id='$id'";
-        if ($conn->query($sql) === TRUE) {
-            //echo "Berhasil Mengupdate Status Sensor </br>";
-        } else {
-            //echo "Error updating record: " . $conn->error;
+        $mail = new PHPMailer; 
+        $mail->IsSMTP();
+        $mail->SMTPSecure = $smtps; 
+        $mail->Host = $smtp; //host masing2 provider email
+        $mail->SMTPDebug = 2;
+        $mail->Port = $port;
+        $mail->SMTPAuth = true;
+        $mail->Username = $email_user; //user email
+        $mail->Password = $pswd; //password email 
+        $mail->SetFrom($email_user,"Head of IT Engginer"); //set email pengirim
+        $mail->Subject = "Alert MONSiS-8 THD"; //subyek email
+        $mail->AddAddress($email_to,"Admin MONSiS-8THD");  //tujuan email
+        $mail->MsgHTML($msg);
+        if($mail->Send()) 
+        {
+            //echo "<br>". $wktu."<br>";
+            //echo $id;
+            $sql = "UPDATE sensor SET send_at='$wktu' WHERE id='$id'";
+            if ($conn->query($sql) === TRUE) {
+                //echo "Berhasil Mengupdate Status Sensor </br>";
+            } else {
+                echo "Error updating record: " . $conn->error;
+            }
+            header("location:setting.php");
         }
-        header("location:setting.php");
+        else echo "Failed to sending message";
     }
-    else echo "Failed to sending message";
 }
 else{
+    $sql1 = "UPDATE sensor SET send_at=Null WHERE status_sensor = 'Normal'";
+    if ($conn->query($sql1) === TRUE) {
+        //echo "Berhasil Mengupdate Status Sensor </br>";
+    } else {
+        echo "Error updating record: " . $conn->error;
+    }
     header('location:setting.php');
 }
 
